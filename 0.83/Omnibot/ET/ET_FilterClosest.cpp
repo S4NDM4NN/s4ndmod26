e2019ef10b5730f3e0be674a7b1d27797b704d7a
Petr Laštovička <plastovicka@users.noreply.github.com>
2025-06-21 20:53:25 +0200

    don't attack the tank if it is mounted by a teammate

--- 0.83/Omnibot/ET/ET_FilterClosest.cpp
+++ 0.83/Omnibot/ET/ET_FilterClosest.cpp
@@ -9,6 +9,35 @@ ET_FilterClosest::ET_FilterClosest(Client *_client, AiState::SensoryMemory::Type
 {
 }
 
+GameEntity GetMountedPlayerOnTank(const MemoryRecord &_record)
+{
+	if(_record.m_TargetInfo.m_EntityFlags.CheckFlag(ET_ENT_FLAG_MOUNTED))
+	{
+		static GameEntity lastVehicle, lastPlayer;
+		static obint32 nextTime = 0;
+		if(lastVehicle == _record.GetEntity() && IGame::GetTime() < nextTime) return lastPlayer;
+		nextTime = IGame::GetTime() + 1000; // Don't check again for a second.
+		lastVehicle = _record.GetEntity();
+
+		IGame::EntityIterator ent;
+		while(IGame::IterateEntity(ent) && ent.GetIndex() < 64)
+		{
+			BitFlag64 flags;
+			Vector3f position;
+			EngineFuncs::EntityPosition(ent.GetEnt().m_Entity, position);
+			if(EngineFuncs::EntityPosition(ent.GetEnt().m_Entity, position)
+				&& SquaredLength(position, _record.GetLastSensedPosition()) < 8000
+				&& InterfaceFuncs::GetEntityFlags(ent.GetEnt().m_Entity, flags)
+				&& flags.CheckFlag(ET_ENT_FLAG_MNT_TANK) && !flags.CheckFlag(ENT_FLAG_DISABLED)) 
+			{
+				return lastPlayer = ent.GetEnt().m_Entity;
+			}
+		}
+		lastPlayer.Reset();
+	}
+	return GameEntity();
+}
+
 bool ET_FilterClosest::CheckEx(const MemoryRecord &_record)
 {
 	// Special consideration for some entity types.
@@ -17,6 +46,8 @@ bool ET_FilterClosest::CheckEx(const MemoryRecord &_record)
 	case ET_CLASSEX_VEHICLE_HVY:
 		{
 			if(m_Client->GetWeaponSystem()->GetCurrentWeaponID() == ET_WP_MOBILE_MG42_SET) return false;
+			GameEntity mounted = GetMountedPlayerOnTank(_record);
+			if(mounted.IsValid() && m_Client->IsAllied(mounted)) return false;
 			break;
 		}
 	case ET_CLASSEX_MG42MOUNT:

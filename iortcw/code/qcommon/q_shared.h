f92c4b0bbee6fac223c1f828780f9ef0cfb79eee
Travis Sandmann <travissandmann@gmail.com>
2026-06-14 17:15:31 -0500

    added s4ndmod to iortcw to differenciate the 2

--- iortcw/code/qcommon/q_shared.h
+++ iortcw/code/qcommon/q_shared.h
@@ -97,7 +97,7 @@ If you have questions concerning this license or the applicable additional terms
 #define LEGACY_HEARTBEAT_FOR_MASTER	"Wolfenstein-1"
 
 #ifndef PRODUCT_VERSION
-  #define PRODUCT_VERSION "1.51d-MP"
+  #define PRODUCT_VERSION "1.51d-S4NDMoD26"
 #endif
 
 #ifndef OLD_PRODUCT_VERSION
